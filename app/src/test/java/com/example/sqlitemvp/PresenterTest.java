75f8986ca5bcab3b847cf4bbf0b5a154ded9be55
RTsunskiy <cunami_27-30@mail.ru>
2019-12-04 17:26:52 +0300

    final for SQLite

---
+++ app/src/test/java/com/example/sqlitemvp/PresenterTest.java
@@ -0,0 +1,36 @@
+package com.example.sqlitemvp;
+
+import com.example.sqlitemvp.presentation.activity.MainActivity;
+import com.example.sqlitemvp.presentation.presenter.Presenter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class PresenterTest {
+
+    @Mock
+    private MainActivity mainActivity;
+
+
+    private Presenter presenter;
+
+
+    @Before
+    public void setUp() {
+        presenter = new Presenter(mainActivity);
+    }
+
+    @Test
+    public void addTaskToTable() {
+        String testNoteName = "test note";
+        verify(presenter).addTaskToTable(testNoteName);
+        verify(presenter).readTaskTable();
+    }
+
+}
